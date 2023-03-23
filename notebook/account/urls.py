78eabbd08d3e62e63a81724af62654eee95a1dfa
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-23 16:56:30 +0300

    Add registration functionality

--- notebook/account/urls.py
+++ notebook/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import BaseRegisterView
 
 urlpatterns = [
     path('login/',
@@ -8,4 +9,7 @@ urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name='account/logout.html'),
          name='logout'),
+    path('signup/',
+         BaseRegisterView.as_view(template_name='account/signup.html'),
+         name='signup'),
 ]

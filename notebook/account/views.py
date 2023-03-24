0dd619d81803aa6cfe3ddd3fe6d66decb47f33ac
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-24 16:23:57 +0300

    Clean up

--- notebook/account/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.views.generic.edit import CreateView
-from .models import BaseRegisterForm
-
-
-class BaseRegisterView(CreateView):
-    model = User
-    form_class = BaseRegisterForm
-    success_url = '/'
--- notebook/p2org/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import AccountView
-
-
-urlpatterns = [
-    path('', AccountView.as_view(), name='account'),
-]
